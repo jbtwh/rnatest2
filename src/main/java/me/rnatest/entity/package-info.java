c0326e66f2f4a584aae652030791dd6f609d3ed6
DEN <dkorn7@ukr.net>
2016-08-17 22:12:20 +0300

    jaxb test

--- src/main/java/me/rnatest/entity/package-info.java
+++ src/main/java/me/rnatest/entity/package-info.java
@@ -1,5 +1,7 @@
 /**
  * Created by DEN
  */
-@javax.xml.bind.annotation.XmlSchema(namespace = "main")
+//@javax.xml.bind.annotation.XmlSchema(namespace = "main", elementFormDefault =  XmlNsForm.QUALIFIED)
 package me.rnatest.entity;
+
+import javax.xml.bind.annotation.XmlNsForm;
--- src/main/java/me/rnatest/entity/test/package-info.java
+++ src/main/java/me/rnatest/entity/test/package-info.java
@@ -1,5 +1,7 @@
 /**
  * Created by DEN
  */
-@javax.xml.bind.annotation.XmlSchema(namespace = "test")
+//@javax.xml.bind.annotation.XmlSchema(namespace = "test2", elementFormDefault =  XmlNsForm.QUALIFIED)
 package me.rnatest.entity.test;
+
+import javax.xml.bind.annotation.XmlNsForm;
