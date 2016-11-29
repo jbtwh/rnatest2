592973dfd90cf42bfded21d14f26247a3f395e4d
DEN <dkorn7@ukr.net>
2016-11-29 18:31:02 +0200

    Revert "jaxb test"
    This reverts commit 1c05bb9e2e55495c70ae341974ab19c7f610aa9b.

--- src/main/java/me/rnatest/entity/package-info.java
+++
@@ -1,5 +0,0 @@
-/**
- * Created by DEN
- */
-@javax.xml.bind.annotation.XmlSchema(namespace = "main")
-package me.rnatest.entity;
--- src/main/java/me/rnatest/entity/test/package-info.java
+++
@@ -1,5 +0,0 @@
-/**
- * Created by DEN
- */
-@javax.xml.bind.annotation.XmlSchema(namespace = "test")
-package me.rnatest.entity.test;
